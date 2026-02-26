f7fdfce61e9271e416b04992f4d3b71655e7de7f
Kaushiki44 <kaushikiofficial44@gmail.com>
2026-02-26 16:38:30 +0530

    nesting, dynamic routing, 404

---
+++ 19-routing-advanced/src/components/Navbar2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom"
+
+const Navbar2 = () => {
+    
+  let navigate = useNavigate()
+
+  return (
+    <div className='py-2 px-5 bg-cyan-800'>
+        <button 
+          onClick={()=>{
+            navigate('/')
+          }}
+          className='font-medium bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer'>
+          Return to Home Page
+        </button>
+        <button 
+          onClick={()=>{
+            navigate(-1)
+          }}
+          className='font-medium bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer'>
+          Back
+        </button>
+      
+    </div>
+  )
+}
+
+export default Navbar2
---
+++ 19-routing-advanced/src/pages/CourseDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useParams} from 'react-router-dom'
+
+const CourseDetails = () => {
+
+    const params = useParams()
+
+  return (
+    <div>
+        <h1> {params.courseId} Course Detail page</h1>
+      
+    </div>
+  )
+}
+
+export default CourseDetails
